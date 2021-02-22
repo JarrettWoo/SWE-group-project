5fb230d35a63293774dcce4525e84047192c2db2
Mark Dacar <markdacar@gmail.com>
2021-02-22 11:47:21 -0500

    Removed debug prints to console and added some comments to the taskManager.py file

--- setup.py
+++ setup.py
@@ -8,12 +8,7 @@ if __name__ == "__main__":
     task_table = taskbook_db.get_table('task')
     task_table.drop()
     task_table = taskbook_db.create_table('task')
-    taskManager.insert_Tasks("Do something useful", status=True, endDt="2022-3-18")
+    taskManager.insert_Tasks("Do something useful", status=True, repeatNum=1, startDt="2021-2-21")
     taskManager.insert_Tasks("Do something fantastic", endDt='2021-2-21')
     taskManager.insert_Tasks("Do something remarkable", endDt='2021-2-20')
     taskManager.insert_Tasks("Do something unusual", status=True, endDt='2021-2-22')
-
-    tasks = [dict(x) for x in task_table.find(order_by='time')]
-    print(len(tasks))
-    for task in tasks:
-        print(task)
