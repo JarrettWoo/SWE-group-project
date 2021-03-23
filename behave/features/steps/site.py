5d94f8d44b6b7b3db0b416f110d78687dc5e29d8
Justin L <78121936+jel2658@users.noreply.github.com>
2021-03-23 16:14:32 -0400

    Update site.py

--- behave/features/steps/site.py
+++ behave/features/steps/site.py
@@ -1,17 +1,23 @@
-# features/steps/webdriver_example.py
-from behave_webdriver.steps import *
+#from behave_webdriver.steps import *
 from behave import *
 import requests
 
 @given(u'we can access taskbook')
 def step_impl(context):
-    context.browser.get('http://bbehnkese.pythonanywhere.com/')
+    context.r = requests.get('http://bbehnkese.pythonanywhere.com/')
 
 @when(u'we visit the URL for the project')
 def step_impl(context):
-    pass #previous step accounts for this
-
+    pass #previous step accounts for this, could change it
 
 @then(u'we get the taskbook page')
 def step_impl(context):
-    pass
+    context.r.status_code
+
+@when(u'we make a task')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we make a task')
+
+@then(u'the task is available')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the task is available')
